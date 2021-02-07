2ad9af1a0377752fb085f8f502401940ee5ae9a1
James Scheiber <jscheiber22@gmail.com>
2021-02-07 14:47:51 -0800

    good updates and works well with multiprocessing and should be as fast as possible in theory

--- duplicatefinder/duplicatefinder.py
+++ duplicatefinder/duplicatefinder.py
@@ -5,6 +5,9 @@ from PIL import Image
 from PIL import ImageChops
 from datetime import datetime
 import subprocess
+from time import sleep
+import multiprocessing as mp
+import re
 
 '''
 
@@ -18,102 +21,94 @@ class Finder:
     def __init__(self):
         self.imageDestination = '/media/sf_CompletedTorrents/Duplicates/'
 
+        self.newPulls = []
+        self.imagePulledCount = 0
+        self.duplicatesFoundCount = 0
+        self.imagesCheckedCount = 0
+
         # COLORS :)
         self.red = '\033[91m'
         self.yellow = '\033[93m'
         self.reset = '\033[0m'
 
     # The main running method that extends out to other methods, but will always return back here to do main stuff
-    def find(self):
-        listPath = 'directories.txt'
-        f = open(listPath, 'r')
-        directories = f.readlines()
-        f.close()
-
-        imagePulledCount = 0
-        newPulls = []
-
+    def pullImages(self, directory):
+        toAdd = ''
         # Pulls image at x minutes (specified in pullImage method) for each video file within 2 directory layers
-        print("\nBeginning to pull images.\n\n")
-        for directory in directories:
-            directory = directory.replace('\n', '').replace('\r', '')
-            if not directory.endswith('/'):
-                directory = directory + '/'
-            if directory is not None and directory != "":
-                print("\nNow checking: " + directory + "\n")
-                directoryContents = os.listdir(directory)
-                for content in directoryContents:
-                    if os.path.isfile(directory + content) and not content.startswith('.'):
-                        if content.lower().endswith('mp4') or content.lower().endswith('mov') or content.lower().endswith('avi') or content.lower().endswith('mkv') or 'mp4' in content.lower(): # checks for video filetype
+        directory = directory.replace('\n', '').replace('\r', '')
+        if not directory.endswith('/'):
+            directory = directory + '/'
+        if directory is not None and directory != "":
+            print("\nNow checking: " + directory + "\n")
+            directoryContents = os.listdir(directory)
+            for content in directoryContents:
+                if os.path.isfile(directory + content) and not content.startswith('.'):
+                    if content.lower().endswith('mp4') or content.lower().endswith('mov') or content.lower().endswith('avi') or content.lower().endswith('mkv') or 'mp4' in content.lower(): # checks for video filetype
+                        if not content.endswith('.torrent'):
                             pulled = self.pullImage(directory, content)
                             if pulled:
-                                newPulls.append(content + '.jpg')
-                                imagePulledCount += 1
-                        # else:
-                            # print(self.yellow + "\n\nNotice: Ignoring " + content + " because it has the wrong file ending :/\n\n" + self.reset)
+                                toAdd = directory[1:-1].replace('/', '.') + '/' + content + '.jpg'
+                                self.newPulls.append(directory[1:-1].replace('/', '.') + '/' + content + '.jpg')
+                                self.imagePulledCount += 1
 
-                    elif os.path.isdir(directory + content):
-                        contentOfDirectoryContent = os.listdir(directory + content)
-                        for subcontent in contentOfDirectoryContent:
-                            if os.path.isfile(directory + content + "/" + subcontent) and not subcontent.startswith('.'):
-                                if subcontent.lower().endswith('mp4') or subcontent.lower().endswith('mov') or subcontent.lower().endswith('avi') or subcontent.lower().endswith('mkv') or 'mp4' in subcontent.lower(): # checks for video filetype
-                                    # do all the file stuff but replace content with subcontent
+                elif os.path.isdir(directory + content):
+                    contentOfDirectoryContent = os.listdir(directory + content)
+                    for subcontent in contentOfDirectoryContent:
+                        if os.path.isfile(directory + content + "/" + subcontent) and not subcontent.startswith('.'):
+                            if subcontent.lower().endswith('mp4') or subcontent.lower().endswith('mov') or subcontent.lower().endswith('avi') or subcontent.lower().endswith('mkv') or 'mp4' in subcontent.lower(): # checks for video filetype
+                                # do all the file stuff but replace content with subcontent
+                                if not subcontent.endswith('.torrent'):
                                     pulled = self.pullImage(directory + content + "/", subcontent) # Sends more accurate directory info due to subcontent's existence
                                     if pulled:
-                                        newPulls.append(subcontent + '.jpg')
-                                        imagePulledCount += 1
+                                        toAdd = directory[1:].replace('/', '.') + content + '/' + subcontent + '.jpg'
+                                        self.newPulls.append(directory[1:].replace('/', '.') + content + '/' + subcontent + '.jpg')
+                                        self.imagePulledCount += 1
+
+            if self.newPulls and self.newPulls[0] is not None:
+                return self.newPulls
 
 
-        if imagePulledCount > 0:
-            print("\n\nPulled " + str(imagePulledCount) + " images.\nNow checking them for duplicates.\nTime Started: " + datetime.now().strftime("%H:%M") + "\n\n")
 
-            duplicatesFoundCount = 0
-            imagesCheckedCount = 0
-            # Now actually does duplicate checking
-            for directory in os.listdir(self.imageDestination): # each directory in duplicates folder
-                for image in os.listdir(self.imageDestination + directory): # each image in each directory
-                    if image in newPulls:
-                        for secondaryDirectory in os.listdir(self.imageDestination): # each directory in duplicates folder to search in for comparing images
-                            for comparingImage in os.listdir(self.imageDestination + secondaryDirectory): # all images each other image is to be compared to
-                                # Although repetitive, it should be updated frequently so that it doesn't reuse any images to kill even more time
-                                f = open('checked.txt', 'r+')
-                                checkedImages = f.readlines()
-                                f.close()
+    def compare(self, image):
+        if image is None or image == '':
+            return
+        imageEnding = re.search('\/.*\.jpg', image).group().replace('/', '')
+        for secondaryDirectory in os.listdir(self.imageDestination): # each directory in duplicates folder to search in for comparing images
+            for comparingImage in os.listdir(self.imageDestination + secondaryDirectory): # all images each other image is to be compared to
+                # Although repetitive, it should be updated frequently so that it doesn't reuse any images to kill even more time
+                f = open('checked.txt', 'r+')
+                checkedImages = f.readlines()
+                f.close()
 
-                                used = False
-                                for line in checkedImages:
-                                    if comparingImage in line:
-                                        used = True
-                                        break
+                used = False
+                for line in checkedImages:
+                    if comparingImage in line:
+                        used = True
+                        break
 
-                                if not image == comparingImage and not used: # avoids it seeing itself duh
-                                    image_one = Image.open(self.imageDestination + directory + "/" + image)
-                                    image_two = Image.open(self.imageDestination + secondaryDirectory + "/" + comparingImage)
+                if imageEnding != comparingImage and not used: # avoids it seeing itself duh
+                    image_one = Image.open(self.imageDestination + image)
+                    image_two = Image.open(self.imageDestination + secondaryDirectory + "/" + comparingImage)
 
-                                    diff = ImageChops.difference(image_one, image_two)
+                    diff = ImageChops.difference(image_one, image_two)
 
-                                    if diff.getbbox() is None:
-                                        # same
-                                        f = open('duplicates.txt', 'a')
-                                        f.write(image + ' from ' + directory + '\n')
-                                        f.close()
-                                        duplicatesFoundCount += 1
-                                        print(self.red + "\n\nDuplicate " + str(duplicatesFoundCount) + " is " + image + ' from ' + directory + self.reset + "\n\n")
-
-                        # After checking an image against all other images, its name is added to the checked list in checked.txt and should not be checked against any others
-                        f = open('checked.txt', 'a')
-                        f.write(image)
+                    if diff.getbbox() is None:
+                        # same
+                        f = open('duplicates.txt', 'a')
+                        f.write("\nDuplicate is " + imageEnding + ' from ' + image + ' which matched with ' + comparingImage + ' from ' + secondaryDirectory + '.' + '\n')
                         f.close()
+                        print(self.red + "\n\nDuplicate is " + imageEnding + ' from ' + image + ' which matched with ' + comparingImage + ' from ' + secondaryDirectory + '.' + self.reset + "\n\n")
 
-                        imagesCheckedCount += 1
-                        print("Checked image #" + str(imagesCheckedCount) + " out of " + str(imagePulledCount) + ", ie " + str(round(100 * (imagesCheckedCount / imagePulledCount), 2)) + "% complete :).")
-
-            print("\nFound " + str(duplicatesFoundCount) + " duplicates.")
-            print("Time Finished: " + datetime.now().strftime("%H:%M") + "\n\n")
-
-        else:
-            print("\nNo images pulled. uho :/\n")
-
+        if imageEnding is not None:
+            # After checking an image against all other images, its name is added to the checked list in checked.txt and should not be checked against any others
+            f = open('checked.txt', 'a')
+            f.write(imageEnding + '\n')
+            f.close()
+            # f = open('checked.txt', 'r')
+            # linecount = f.readlines()
+            # f.close()
+            # print('Comparison ' + str(len(linecount)) + ' out of ' + str(len(newPulls)) + ' completed.')
+            print('Another one checked.')
 
     def pullImage(self, directory, content):
         imageDestination = self.imageDestination
@@ -148,8 +143,27 @@ class Finder:
 
 
 
-
 if __name__ == "__main__":
     finder = Finder()
-    finder.find()
+    pool = mp.Pool(6)
+    newPulls = []
+
+    listPath = 'directories.txt'
+    f = open(listPath, 'r')
+    directories = f.readlines()
+    f.close()
+
+    directories = [i for i in directories if not i.startswith('#')]
+
+    newPullsyucky = pool.map(finder.pullImages, directories)
+    for pull in newPullsyucky:
+        if pull is not None:
+            newPulls += pull
+    print("\n\nPulled " + str(len(newPulls)) + " images.\nNow checking them for duplicates.\nTime Started: " + datetime.now().strftime("%H:%M") + "\n\n")
+
+    if len(newPulls) > 0:
+        pool.map(finder.compare, newPulls)
+
+    print("Time Finished: " + datetime.now().strftime("%H:%M") + "\n\n")
+
     finder.resetChecked()
